2cf59a9b157bb4ae43544339a6e8ef928b010352
Zeljko <zeljko.romanovic@mstworld.com>
2020-04-20 12:50:13 +0100

    fix: removed debug logs

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,6 @@ async function run() {
                 requested_visibility
             }
         });
-        core.debug(item.url);
-        core.debug(link_password);
         
         core.setOutput(OUTPUT_DROPBOX_LINK, item.url);
         core.setOutput(OUTPUT_DROPBOX_LINK_PASSWORD, link_password);
